1426f4419a8ffa4eeb0f306df6202e170ec31bf2
Taylor Vesly <taylorvesly28@gmail.com>
2023-04-03 14:30:48 +1000

    Update pseudo code

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ function getComputerChoice() {
 
     // This logic assigns random numbers 1 - 3 to scissor, paper, and rock values
     if (randomNum === 1) {
-        computerChoice === 'Scissors'
+        computerChoice === Scissors
     }
     else if (randomNum === 2) {
-        computerChoice === 'Paper'
+        computerChoice === Paper
     }
     else if (randomNum === 3) {
-        computerChoice === 'Rock'
+        computerChoice === Rock
     }
     else {
         return 'Error with computerChoice function'
@@ -45,8 +45,29 @@ function getComputerChoice() {
 
 3. Create a function playRound() which takes both the user and computers choices as parameters and calculates the winner:
 
-function playRound(user, computer) {
+function playRound(userSelection, computerSelection) {
+    
+    // Winning conditions for the user
+    if (userSelection === Scissors && computer Selection === Paper) {
+        return 'You win! Scissors beats Paper'
+    } 
+    else if (userSelection === Paper && computerSelection === rock) {
+        return 'You win! Paper beats Rock'
+    }
+    else if (userSelection === Rock && computerSelection === Scissors) {
+        return 'You win! Rock beats Scissors'
+    }
+    
+    // Tie scenario
+    else if (userSelection === computerSelection) {
+        return 'Tie!'
+    }
 
+    // If the above conditions aren't true then you lose
+    else {
+        return 'You lose!'
+    }
+    
 }
 --------------------------------------------------------------------------------
 
@@ -55,9 +76,3 @@ function playRound(user, computer) {
 5. 
 
 */
-
-function test() {
-  return Math.floor(Math.random() * 3 + 1);
-}
-
-console.log(test());
